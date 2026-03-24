c151fd8b7977995e83e1630e40b2abf01a84691f
red-Ruso <manueltroncosovargas@icloud.com>
2026-03-24 19:08:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 print("Hola, soy Manuel")
+
+zelda = {
+    "Link": "El protagonista, es un caballero y no habla",
+    "Zelda": "Es la princesa y Link tiene que salvarla",
+    "Ganon": "Es el malo y quiere tener el reino de Zelda"
+}
+
+pregunta = input("Escribe la palabra que no entiendas con mayuscula en la primera letra")
+
+if pregunta in zelda.keys():
+    print(zelda[pregunta])
+else:
+    print("La palabra no esta en el dicionario")
